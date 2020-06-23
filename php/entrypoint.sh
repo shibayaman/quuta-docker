63bdcae86ed74fe85c9331a77cf29f4bc0026ea5
shibayaman <shibayaman7374@gmail.com>
2020-06-23 23:01:39 +0900

    Initial Commit

---
+++ php/entrypoint.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -e
+
+file_count=`ls -l /var/www/html 2>/dev/null | sed /^total/d | wc -l`
+if [ "${file_count}" != 0 ]
+then
+	echo "skipping create phase"
+else
+	echo "create laravel project"
+	composer create-project laravel/laravel=6.* .  --prefer-dist
+fi
+
+exec "$@"
+
